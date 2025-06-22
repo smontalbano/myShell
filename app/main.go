0b4d11b8ee44b02e0775b34fe48c3d122c3e99f7
smontalbano <sdm030@gmail.com>
2025-06-22 16:14:42 -0400

    Fix typo in bufio.NewReader

--- app/main.go
+++ app/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
-	command, err := bufio.NewREader(os.Stdin).ReadString('\n')
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error reading input: ", err)
 		os.Exit(1)
