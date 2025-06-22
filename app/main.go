306dc2ba9e1ac6caf8d14ce94607350ec6b10286
smontalbano <sdm030@gmail.com>
2025-06-22 16:18:26 -0400

    Implement REPL to main function

--- app/main.go
+++ app/main.go
@@ -7,12 +7,14 @@ import (
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading input: ", err)
-		os.Exit(1)
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input: ", err)
+			os.Exit(1)
+		}
+		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
-	fmt.Println(command[:len(command)-1] + ": command not found")
 }
